788a89f16976b49b5ee75fcaa058382362b5e343
Libra420T <ryantf420@gmail.com>
2026-05-07 18:45:11 -0400

    Update Flags

--- device-akita.mk
+++ device-akita.mk
@@ -8,25 +8,10 @@
 # Disable Artifact Requirements
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 
-# GMS/GAPPS
-WITH_GMS := true
-
 TARGET_SUPPORTED_REFRESH_RATES := 60,120
-
-SURFACE_FLINGER_BOOST := true
-$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
-$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
 
 # Always use scudo for memory allocator
 PRODUCT_USE_SCUDO := true
-
-TARGET_OPTIMIZED_DEXOPT := true
-
-# Build Pixel Launcher
-TARGET_INCLUDE_PIXEL_LAUNCHER := true
-
-# Include Google Telephony
-WITH_GMS_COMMS_SUITE := true
 
 # Include Chargeing Bypass
 BYPASS_CHARGE_SUPPORTED := true
